3718e6e90357fd3482ab450a8a02b8cad40cbc88
Konstantin <kbanin@yandex.ru>
2024-05-13 14:15:02 +0300

    Автоматическая загрузка

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,37 @@
+
+// function ajaxSend (pageNumber)
+
+//    { 
+
+//       $.ajax({
+   
+//       url: 'main/test',
+//       type: 'get',
+//       data: { 'page': pageNumber }, // Передаем номер страницы в виде параметра GET
+//       success: function(res){
+//          // $('#result').html(res);
+//          $('#products-container').html(res);
+   
+//          // console.log(res);
+//       },
+//       error: function (){
+//          alert ('Error!');
+//       }
+//       });
+
+// }
+   
+
+
+
+
+
+
 $(function(){
+  
+   
+   // ajaxSend(1);
+
    $('.pagination a'). click (function(event){
    event.preventDefault(); // Предотвращаем перезагрузку страницы по умолчанию
    var pageNumber = $(this).attr('href').split('=')[1]; // Получаем номер страницы из атрибута data-page элемента, по которому был совершен клик
