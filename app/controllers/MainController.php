2cf861c3143c6fb46be2c5c9f7b9ee5c0b2524d2
Konstantin <kbanin@yandex.ru>
2024-05-22 14:15:01 +0300

    Автоматическая загрузка

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -20,48 +20,41 @@ class MainController extends AppController
 
    public function indexAction()
    {
-      $rssUrls = array(
-         'https://coinspot.io/feed/',
-         'https://ru.investing.com/rss/news_301.rss',
-         'https://cryptocurrency.tech/feed/',
-         'https://bits.media/rss2/',
-         'https://cryptofeed.ru/feed/',
-         'https://ru.beincrypto.com/feed/',
-         'https://2bitcoins.ru/feed/',
-        
-     );
-     
-     $titles = [];
-     $links = [];
-     $dates = [];
-     $descriptions = [];
-     $totalItemCount = 0; // Общее количество новостей
-
-     foreach ($rssUrls as $rssUrl) {
-      $rss = simplexml_load_file($rssUrl);
-
-      $itemCount = count($rss->channel->item); // Количество новостей в текущей ленте
-      $totalItemCount += $itemCount; // Добавляем количество новостей к общему счетчику
-      
+      $crypto = new Main;
   
+      $titles = [];
+      $links = [];
+      $dates = [];
+      $descriptions = [];
 
-     
 
-      foreach ($rss->channel->item as $item) {
-        
-         (string)$title = $item->title;
-         (string)$link = $item->link;
-         (string)$date = $item->pubDate;
-         (string)$description = $item->description;
-         
+   //  Получение новостей из массива ссылок  RSS 
+      foreach ($crypto->rssUrls as $rssUrl) {
+         $rss = simplexml_load_file($rssUrl);
 
-         $titles[] = $title;
-         $links[] = $link;
-         $dates[] = $date;
-         $descriptions[] = $description;
+         foreach ($rss->channel->item as $item) {
+
+            (string)$title = $item->title;
+            (string)$link = $item->link;
+            (string)$date = $item->pubDate;
+            (string)$description = $item->description;
+
+
+            $titles[] = $title;
+            $links[] = $link;
+            $dates[] = $date;
+            $descriptions[] = $description;
+         }
       }
-   }
-      debug($totalItemCount);
+
+       //Сохранение данных новостей в БД таблицу cryptonews в случае добавление новой ссылки в массив rssUrls
+      if (count($crypto->rssUrls) >= 10){
+      $crypto->addDataToDatabase($titles,$links,$dates,$descriptions);}
+
+      //Обновление данных (скрипт запускается каждый 6 часов с использованием cron)
+      $crypto->updateDateToDatabase($titles,$links,$dates,$descriptions);
+
+
       $this->set(compact('titles', 'links', 'dates', 'descriptions'));
    }
 
--- app/models/Main.php
+++ app/models/Main.php
@@ -8,9 +8,21 @@ use fw\core\base\Model;
 
 class Main extends Model
 {
+    public $rssUrls = array(
+        'https://coinspot.io/feed/',
+        'https://ru.investing.com/rss/news_301.rss',
+        'https://cryptocurrency.tech/feed/',
+        'https://bits.media/rss2/',
+        'https://cryptofeed.ru/feed/',
+        'https://ru.beincrypto.com/feed/',
+        'https://2bitcoins.ru/feed/',
+        'https://forklog.com/feed',
+        'https://www.coindesk.com/arc/outboundfeeds/rss/?_gl=1*1gmicje*_up*MQ..*_
+        ga*MjEzOTc3NjMwMC4xNzE2Mjk1MjYx*_ga_VM3STRYVN8*MTcxNjI5NTI2MS4xLjAuMTcxNjI5NTI2MS4wLjAuMTIyODIwMDQxMQ..',
+        
+    );
 
-
-
+    
 
     public function getCryptoArray()
     {
@@ -30,5 +42,35 @@ class Main extends Model
 
     }
 
-    
+// Добавление данных
+    public function addDataToDatabase($titles, $links, $dates, $descriptions)
+    {       
+           foreach ($titles as $index=>$title){
+            $news = \R::dispense('cryptonews');
+            $news->title = $title;
+            $news->description = $descriptions[$index];
+            $news->dates = date('Y-m-d', strtotime($dates[$index]));
+            $news->links = $links[$index];
+            \R::store($news);
+
+           }
+           return $news;
+}
+
+//Обновление данных
+public function updateDateToDatabase ($titles, $links, $dates, $descriptions){
+    $news = \R::findAll('cryptonews');
+    foreach ($news as $index => $item) {
+        
+        $item->title = $titles[$index-1];
+        $item->description = $descriptions[$index-1];
+        $item->dates = date('Y-m-d', strtotime($dates[$index-1]));
+        $item->links = $links[$index-1];
+        \R::store($item);
+    }
+    return $news;
+
+}
+
+
 }
